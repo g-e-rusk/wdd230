75fe22a433562dcb554a6e4e7b96c5ca5216211e
g-e-rusk <grusk92@gmail.com>
2023-01-07 19:30:17 -0800

    Updated JS for page

--- script/resultdate.js
+++ script/resultdate.js
@@ -1,6 +1,6 @@
 const year = document.querySelector('#currentyear');
 year.textContent = `${new Date().getFullYear()}`;
 
-const todaysdate = new Date();
-document.getElementById("resultdate").innerHTML = todaysdate
+const oLastModif = new Date(document.lastModified);
+document.getElementById("resultdate").innerHTML = oLastModif
 

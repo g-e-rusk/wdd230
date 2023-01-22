b6266eb9da8700fdd88c31c52bc7be252481537e
g-e-rusk <grusk92@gmail.com>
2023-01-21 18:02:12 -0800

    updated assignment portal and js

--- lesson3/scripts/wddate.js
+++
@@ -1,5 +0,0 @@
-const date = new Date();
-let year = date.getFullYear();
-document.querySelector("#currentyear").innerText = year;
-
-document.querySelector("#date").textContent = document.lastModified;

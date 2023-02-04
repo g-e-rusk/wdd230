abd53dce30b0b24e93a0ebf071264da7e2affced
g-e-rusk <grusk92@gmail.com>
2023-02-04 12:32:21 -0800

    updated chamber site

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -16,3 +16,14 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
     dateStyle: "full"
 }).format(now);
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`
+
+const date = now.getDay();
+if (date == 1 || date == 2) {
+    document.querySelector(".banner").style.display = "block";
+}else {
+    document.querySelector(".banner").style.display = "none";
+};
+
+    document.querySelector(".banner-close").addEventListener("click", function() {
+        this.closest(".banner").style.display = "none";
+});
